d4858b9cc345b6c15e4f4878b204e478435539e6
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-04-16 22:19:01 -0300

    last fixes 20240416

--- Frontend/HardwareHaven/src/main.ts
+++ Frontend/HardwareHaven/src/main.ts
@@ -1,13 +1,11 @@
 /// <reference types="@angular/localize" />
-<<<<<<< HEAD:Frontend/HarwareHaven/src/main.ts
-=======
 
 import { bootstrapApplication } from '@angular/platform-browser';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
->>>>>>> Goingon20240411:Frontend/HardwareHaven/src/main.ts
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { HomeComponent } from './app/home/home.component.js';
-bootstrapApplication(HomeComponent)
+
+
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
